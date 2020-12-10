79f0a5605ed824939de2a5724d8ea10a78f8b295
metouch <1101642512@qq.com>
2020-12-10 18:45:02 +0800

    完善题解10

--- isMatch_10.c
+++ isMatch_10.c
@@ -21,14 +21,26 @@ bool isMatch(char * s, char * p){
     if (*p == '\0') {
         return *s == '\0';
     }
-    bool matched = (*s == *p) || (*p == '.');
-    if (*p == '*') {
-        return isMatch()
+    bool matched = *s && (*s == *p || *p == '.');
+    if (*(p + 1) == '*') {
+        return isMatch(s, p + 2) || (matched && isMatch(++s, p));
     } else {
         return matched && isMatch(++s, ++p);
     }
 }
 
+/**
+ * 动态规划思路：
+ * 令f[i][j]为字符串s前i个字符与字符串p前j个字符串的匹配结果，即f[0][0]表示空字符串s与空字符串p的匹配结果
+ * f[0][j]表是空字符串s与字符串p[0..j]的匹配结果
+ * 当p[j-1] == '*'时, 且s[i-1] != p[j-2],则f[i][j] = f[i][j - 2]
+ * 当p[j-1] == '*'时, 且s[i-1] == p[j-2],则f[i][j] = f[i][j - 2] || f[i-1][j]
+ * 当p[j-1] != '*'时, 且s[i-1] == p[j-1]，则f[i][j] = f[i-1][j-1]
+ * 否则f[i][j] = false;
+ * @param s
+ * @param p
+ * @return
+ */
 bool isMatch1(char * s, char * p){
     int sLen = (int)strlen(s);
     int pLen = (int)strlen(p);
@@ -55,8 +67,8 @@ bool isMatch1(char * s, char * p){
 }
 
 int main(int argc, char *argv[]) {
-    char *s = "aa";
-    char *p = "a";
-    printf("%d\n", isMatch(s, p));
+    char *s = "mississippi";
+    char *p = "mis*is*p*.";
+    printf("%d\n", isMatch1(s, p));
     return 0;
 }
