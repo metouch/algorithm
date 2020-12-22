9916ef2aaf160e9d0df72156f8bed0e6505d4046
me_touch <1101642512@qq.com>
2020-12-22 23:39:23 +0800

    完善题15

--- 15_threeSum.c
+++ 15_threeSum.c
@@ -5,10 +5,15 @@
 #include <stdlib.h>
 #include "thsort.h"
 
-void threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
-    int m = numsSize / 3;
+int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+    int m = numsSize * numsSize;
     int n = 3;
-    int p[m][n];
+    if (m < 1) {
+        *returnSize = 0;
+        return NULL;
+    }
+    int **p = (int **)malloc(sizeof(int *) * m);
+    int *columnSize = (int *)malloc(sizeof(int) * m);
     *returnSize = 0;
     quickSort(nums, 0, numsSize - 1);
     for (int i = 0; i < numsSize - 2; ++i) {
@@ -21,33 +26,47 @@ void threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes)
             } else if (sum > 0) {
                 k --;
             } else {
-                printf("[%d, %d, %d]\n", nums[i], nums[j], nums[k]);
-                while ((j + 1 < k) && (nums[j] == nums[j + 1])){
+                p[*returnSize] = (int *)malloc(sizeof(int) * 3);
+                p[*returnSize][0] = nums[i];
+                p[*returnSize][1] = nums[j];
+                p[*returnSize][2] = nums[k];
+                columnSize[*returnSize] = n;
+                while (nums[j] == nums[j + 1]){
                     j++;
                 }
                 j++;
-                while ((k - 1 > j) && (nums[k] == nums[k - 1])){
+                while (nums[k] == nums[k - 1]){
                     k--;
                 }
                 k--;
+                *returnSize += 1;
             }
         }
         while (i + 1 < numsSize - 2 && (nums[i] == nums[i + 1])){
             i++;
         }
     }
+    *returnColumnSizes = columnSize;
+    return p;
 }
 
 int main(){
     int *a = (int *)malloc(sizeof(int) * 5);
-    a[0] = 0;
+    a[0] = -2;
     a[1] = 0;
-    a[2] = 0;
-    a[3] = 0;
-    a[4] = 0;
-    a[5] = 0;
-    int *p[3];
+    a[2] = 1;
+    a[3] = 1;
+    a[4] = 2;
     int returnSize = 0;
-    threeSum(a, 6, &returnSize, NULL);
-
+    int **returnColumnSize = (int **)malloc(sizeof(int *));
+    int** p = threeSum(a, 5, &returnSize, returnColumnSize);
+    for (int i = 0; i < returnSize; ++ i) {
+        int size = *(*returnColumnSize + i);
+        printf("[");
+        for (int j = 0; j < size; ++j) {
+            printf("%d, ", p[i][j]);
+        }
+        printf("]");
+        printf("\n");
+    }
 }
